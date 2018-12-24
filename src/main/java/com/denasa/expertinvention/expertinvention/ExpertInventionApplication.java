5f7bdfdc7c7ae4dbb888365dd4db8758f794b82f
Swapneel Patil <swapneel.patil08@gmail.com>
2018-12-24 22:53:41 +0530

    Added Project.

---
+++ src/main/java/com/denasa/expertinvention/expertinvention/ExpertInventionApplication.java
@@ -0,0 +1,14 @@
+package com.denasa.expertinvention.expertinvention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertInventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertInventionApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/denasa/expertinvention/expertinvention/ExpertInventionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.denasa.expertinvention.expertinvention;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpertInventionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
