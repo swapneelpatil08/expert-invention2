9776b0be815239eb78efa1e99c1774643b1a5a5e
Swapneel Patil <swapneel.patil08@gmail.com>
2018-12-25 15:53:11 +0530

    Changed Application directory.

--- src/main/java/expertinvention/MainController.java
+++ src/main/java/expertinvention/MainController.java
@@ -1,4 +1,4 @@
-package com.denasa.expertinvention.expertinvention;
+package expertinvention;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
